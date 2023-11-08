62866f1a0de00e81bc3e926204552781b6576fd1
saddysk <sksaddamh08@gmail.com>
2023-11-08 14:27:30 +0530

    clean code

--- src/config/azure-openai.config.ts
+++ src/config/azure-openai.config.ts
@@ -9,22 +9,3 @@ const azureOpenAi = new OpenAIClient(
 );
 
 export default azureOpenAi;
-
-// const messages = [
-//   { role: "system", content: "You are a helpful assistant." },
-//   { role: "user", content: "Does Azure OpenAI support customer managed keys?" },
-// ];
-
-// async function main() {
-//   console.log("== Chat Completions Sample ==");
-
-//   const client = new OpenAIClient(
-//     endpoint,
-//     new AzureKeyCredential(azureApiKey)
-//   );
-//   const result = await client.getChatCompletions(deploymentId, messages, {});
-
-//   for (const choice of result.choices) {
-//     console.log(choice.message.content);
-//   }
-// }
