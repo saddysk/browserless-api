9805c99d044b0c0395e1db05e7a5e3c426256aa5
saddysk <sksaddamh08@gmail.com>
2023-12-04 00:56:53 +0530

    update: style info

--- src/modules/summary/prompts/helpers/writing-styles.ts
+++ src/modules/summary/prompts/helpers/writing-styles.ts
@@ -2,7 +2,7 @@ export const writingStyles = {
   None: "",
   "Naval Ravikant": `a philosopher who emphasizes long-term wealth, happiness, rational decisions, minimalism, and mindfulness. He communicates with clarity in concise tweets and podcasts, making his wisdom accessible and influential.`,
   "Elon Musk": `a visionary entrepreneur with a commitment to innovation and sustainability. He communicates directly and informally through social media, with a strong technical background and a bold, responsive style.`,
-  "Balaji Srinivasan": `a decentralization advocate with a strong focus on startups and virtual communities. He communicates with technical expertise across various platforms and often discusses futuristic concepts.`,
-  "Joe Rogan": `an independent, open-minded podcaster with eclectic interests. He conducts long-form, conversational interviews with diverse guests, valuing authenticity and empathy in his approach.`,
-  "Jordan Peterson": `a philosopher and psychologist who emphasizes individualism and free speech. He communicates with a calm, academic style and is known for his best-selling books and public lectures.`,
+  Balaji: `a decentralization advocate with a strong focus on startups and virtual communities. He communicates with technical expertise across various platforms and often discusses futuristic concepts.`,
+  "Yuval Noah Harari": `a renowned historian and author, famous for his books "Sapiens", "Homo Deus", and "21 Lessons for the 21st Century". He specializes in analyzing historical trends and their implications for the future, particularly in technology and society.`,
+  "Tim Ferriss": `an entrepreneur and author, best known for "The 4-Hour Workweek" and his podcast, "The Tim Ferriss Show," focusing on productivity and lifestyle optimization.`,
 };
