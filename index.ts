19e82764a7c1d9d524a1d2d76c5a3b9cb9d5869f
saddysk <sksaddamh08@gmail.com>
2023-12-03 15:02:47 +0530

    fix: increase json limit

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const CONFIG = AppConfig();
 const app: Express = express();
 const PORT = CONFIG.PORT || 3010;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
