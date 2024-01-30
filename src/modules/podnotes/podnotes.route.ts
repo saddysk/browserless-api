8e01f505109c1a288561f2cf39fee914ad3a716d
saddysk <sksaddamh08@gmail.com>
2024-01-30 13:23:07 +0530

    push srt api for podnotes

--- src/modules/podnotes/podnotes.route.ts
+++ src/modules/podnotes/podnotes.route.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from "express";
 import { rssFeedService } from "./services/rss-feed.service";
+import { srtService } from "./services/srt.service";
 
 const podnotesRoute = express.Router();
 
 podnotesRoute.post("/rss-feed", async (req: Request, res: Response) => {
   const response = await rssFeedService(req);
+  res.status(response["status"]);
+  res.send(response);
+});
+
+podnotesRoute.post("/srt", async (req: Request, res: Response) => {
+  const response = await srtService(req);
   res.status(response["status"]);
   res.send(response);
 });
---
+++ src/modules/podnotes/services/srt.service.ts
@@ -0,0 +1,27 @@
+import { Request } from "express";
+import { IResponse } from "../../../interfaces/response.interface";
+
+export const srtService = async (req: Request): Promise<IResponse> => {
+  const { data: rawData } = req.body;
+
+  //   const rawData = JSON.parse(data);
+
+  const paragraphs =
+    rawData.results.channels[0].alternatives[0].paragraphs.paragraphs;
+
+  const srtFormatedString = paragraphs.map((para: any) => {
+    const sentence = para.sentences.map((item: any) => item.text).join(" ");
+
+    return {
+      speaker: para.speaker,
+      start: para.start,
+      end: para.end,
+      text: sentence,
+    };
+  });
+
+  return {
+    status: 200,
+    body: { data: srtFormatedString },
+  };
+};
